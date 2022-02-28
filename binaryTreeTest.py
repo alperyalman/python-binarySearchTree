8052c48b902845c3892c7484fb9fe714a8cb13d7
Alper Yalman <alperyalman@gmail.com>
2022-02-28 13:11:14 +0300

    Test script created

---
+++ binaryTreeTest.py
@@ -0,0 +1,13 @@
+from BinarySearchTree import *
+
+# Create new binary search tree
+bst = BST() 
+node_values = [7, 5, 1, 8, 3, 6, 0, 9, 4, 2]
+
+# Add node values to binary search tree, first value will be the root
+for val in node_values:
+    bst.createNode(val)
+
+# Display binary search tree
+bst.display()
+
